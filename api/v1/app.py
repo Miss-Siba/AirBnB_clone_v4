2c84bdc61a3e874071e24a347280b606f9b23f86
Siba <sibaquma@gmail.com>
2024-05-01 04:52:56 -0700

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""
+app
+"""
 from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
-
 from api.v1.views import app_views
 from models import storage
 
